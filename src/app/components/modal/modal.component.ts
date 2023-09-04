14ca8e31c3fcee9f1e2267478e63de62473010cc
Anna Maksimova <anna.j.maksimova@mail.ru>
2023-09-04 14:35:51 +0300

    Edit Mode update

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EditModeService } from 'src/app/services/edit-mode.service';
 import {ModalService} from 'src/app/services/modal.service'
 
 @Component({
@@ -8,7 +9,8 @@ import {ModalService} from 'src/app/services/modal.service'
 })
 export class ModalComponent implements OnInit{
 
-  constructor(public modalService: ModalService) { }
+  constructor(public modalService: ModalService,
+    public EditModeService: EditModeService) { }
 
   ngOnInit(): void {
   }
--- src/app/services/edit-mode.service.ts
+++ src/app/services/edit-mode.service.ts
@@ -2,6 +2,8 @@
 
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Input } from '@angular/core';
+import { INote } from '../models/inote';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,13 @@ export class EditModeService {
 
   constructor() { }
 
+  @Input() note!: INote;
+
   editMode$ = new BehaviorSubject<boolean>(false)
 
-  open() {
+  open(_note: INote) {
     this.editMode$.next(true)
+    this.note = _note
   }
 
   close() {
